a6be9198b8e4979a0563558fe6e3983fd40eb875
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-04-25 01:21:53 +0200

    modified index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+
+// PAIR PROGRAMMING:
+
+
+// VARIABLES --------------------------------------------------------------------
+
+// let variableSinValor
+
+
+// let booleano1 = true
+// let booleano2 = true
+
+
+// const PI = 3.14
+
+
+// const TAU = 2PI
+
+
+// BOOLEANOS --------------------------------------------------------------------
+
+// let booleanoAnd = (booleano1 = true) === (booleano2 = false)
+
+
+// let booleano1 = true
+// let booleanoNot = !booleano1
+
+
+// let booleano1=true
+// let booleano2=true
+
+
+// let booleanoMix0 = (booleano1 || booleano2) &&  (booleano1 || (!booleano1 && !booleano2))
+
+
+// OPERADORES --------------------------------------------------------------------
+
+// let incrementarDesp = 2
+// let resultadoDesp = incrementarDesp++
+
+
+// let incrementarAntes = 2
+// let resultadoAntes = ++incrementarAntes
+
+
+// BUCLES --------------------------------------------------------------------
+
+// let contarHasta10_2;
+// for (contarHasta10_2 = 0; contarHasta10_2 <= 10; contarHasta10_2++) {
+//     console.log(contarHasta10_2)
+//  }
+
+
+// let postI = 0
+// let postJ = 0
+
+
+// for (i = 0; i < 11; i++) {
+//     postI = postI + postJ++
+// }
+
+
+// let sumaPares = 0
+// for (let i = 0; i<=10; i++){
+//     console.log(i)
+//     if (i%2 === 0){
+//         console.log(sumaPares + i)       
+//     }   
+// }
+
+
+
+// INDIVIDUAL PROGRAMMING:
+
+// VARIABLES --------------------------------------------------------------------
+
+let variableValorNumerico = 31
+
+
+const MiNombre = Dani
+
+const MiNumeroFav = 5
+
+
+// BOOLEANOS --------------------------------------------------------------------
+
+let booleanoOr = (booleano1 = true) || (booleano2 = false)
+
+
+
+
