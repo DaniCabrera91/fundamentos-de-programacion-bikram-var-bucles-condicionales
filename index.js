ee8ef436def71d3845bef77fb49dfe8d48a659ee
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-04-25 16:37:18 +0200

    Updated content of index.js file

--- index.js
+++ index.js
@@ -2,173 +2,107 @@
 // PAIR PROGRAMMING:
 
 
-// VARIABLES --------------------------------------------------------------------
+// VARIABLES 
 
-// 1.- Crear variable tipo let de nombre variableSinValor declarada sin valor
-// let variableSinValor
-
-
-// 2.- Crear 2 variables tipo let de nombres booleano1 y booleano2 con valores booleanos
-// let booleano1 = true
-// let booleano2 = true
+let variableSinValor
 
 
-// 3.- Crear variable tipo const de nombre PI declarada con valor 3.14
-// const PI = 3.14
+let booleano1 = true
+let booleano2 = false
 
 
-// 4.- Crear variable tipo const de nombre TAU declarada con valor 2 veces PI
-// const TAU = 2PI
+const PI = 3.14
 
 
-// BOOLEANOS --------------------------------------------------------------------
-
-// 5.- Crear variable booleanoAnd cuyo valor sea la comparación booleana booleano1 and booleano2
-// let booleanoAnd = (booleano1 = true) === (booleano2 = false)
+const TAU = PI * 2
 
 
-// 6.- Crear variable booleanoNot cuyo valor sea la compracación booleana no booleano1
-// let booleano1 = true
-// let booleanoNot = !booleano1
+// BOOLEANOS 
+
+let booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
+
+let booleanoAnd = booleano1 == booleano2
+
+let booleanoNot
+booleanoNot = ! booleano1
 
 
-// 7.- Crear variable booleanoMix0 cuyo valor sea la compración booleana (booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)
-// let booleano1=true
-// let booleano2=true
-// let booleanoMix0 = (booleano1 || booleano2) &&  (booleano1 || (!booleano1 && !booleano2))
+// OPERADORES 
+
+let incrementarDesp = 2
+let resultadoDesp = incrementarDesp ++
+
+let incrementarAntes = 2
+let resultadoAntes = ++ incrementarAntes
+
+// BUCLES 
+
+let contarHasta10_2 = 0
+for ( let i=0; i < 10; i++ ) {
+    contarHasta10_2++
+}
+
+let postI = 0
+let postJ = 0
+for (let i = 0; i < 11; i++) {
+    postI = postI + postJ++
+}
 
 
-// OPERADORES --------------------------------------------------------------------
-
-// 8.- Crear variable incrementarDesp con valor 2 y asigna su valor con postincremento a resultadoDesp
-// let incrementarDesp = 2
-// let resultadoDesp = incrementarDesp++
-
-
-// 9.- Crear variable incrementarAntes con valor 2 y asigna su valor con preincremento a resultadoAntes
-// let incrementarAntes = 2
-// let resultadoAntes = ++incrementarAntes
-
-
-// BUCLES --------------------------------------------------------------------
-
-// 10.- Crear variable contarHasta10_2 con valor 0 e incrementar su valor con un bucle for hasta que se verifique que contarHasta10_2 === 10
-// let contarHasta10_2;
-// for (contarHasta10_2 = 0; contarHasta10_2 <= 10; contarHasta10_2++) {
-//     console.log(contarHasta10_2)
-//  }
-
-
-// 11.- Crear las variables postI y postJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de postI el valor de postJ++
-// let postI = 0
-// let postJ = 0
-// for (i = 0; i < 11; i++) {
-//     postI = postI + postJ++
-// }
-
-
-// 12.- Crear la variable sumaPares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es par se deberá sumar a sumaPares el número de la iteración actual (i)
-// let sumaPares = 0
-// for (let i = 0; i<=10; i++){
-//     console.log(i)
-//     if (i%2 === 0){
-//         console.log(sumaPares + i)       
-//     }   
-// }
+let sumaPares = 0
+for (let i = 0; i<10; i++){
+    if (i % 2 === 0)
+        sumaPares = sumaPares + i   
+    }   
 
 
 // INDIVIDUAL PROGRAMMING:
 
-// VARIABLES --------------------------------------------------------------------
+// VARIABLES 
 
-// 13.- Crear variable tipo let de nombre variableValorNumerico declarada con un valor numérico cualquiera
-// let variableValorNumerico = 31
-
-
-// 14.- Crear variable tipo const de nombre MiNombre declarada con valor tu nombre
-// const MiNombre = "Dani"
+let variableValorNumerico = 31
 
 
-// 15.- Crear variable tipo const de nombre MiNumeroFav declarada con valor numérico
-// const MiNumeroFav = 5
+const MiNombre = 'Dani'
 
 
-// BOOLEANOS --------------------------------------------------------------------
+const MiNumeroFav = 5
 
-// 16.- Crear variable booleanoOr cuyo calor sea la comparación booleana booleano1 or booleano2
-// let booleanoOr = (booleano1 = true) || (booleano2 = false)
+// BOOLEANOS 
 
+let booleanoOr = booleano1 || booleano2
 
-// 17.- Crear variable booleanoMix1 cuyo valor sea la comparación booleana (booleano1 and (TAU/2 sea igual a PI)) or (variableValorNumerico mayor o igual que MiNumeroFav)
-// let booleano1 = true
-// let PI = 3.14
-// let TAU = 2
-// TAU2 = TAU*2
-// let variableValorNumerico = 1
-// let MiNumeroFav = 5
-// let booleanoMix1 = (booleano1 && TAU2 == PI) || (variableValorNumerico >= MiNumeroFav)
+let booleanoMix1 = (booleano1 && (TAU/2 == PI)) || (varianbleValorNumerico <= MiNumeroFav)
 
+const seisNoEsNueve = 6 != 9
 
-// 18.- Crear variable seisNoEsNueve cuyo valor sea la comparación booleana 6 no es estrictamente igual que 9
-// const seisNoEsNueve = (6 != 9)
+const booleanoMix2 = (variableValorNumerico > 0 || (MiNumeroFav * TAU)); 
 
+// OPERADORES 
 
-// 19.- Crear variable booleanoMix2 cuyo valor sea la comparación booleana variableValorNumerico positivo (0 no incluido) o menor que -(MiNumeroFav * TAU)
-// const variableValorNumerico = 1
-// const MiNumeroFav = 5
-// const TAU = 2
-// const booleanoMix2 = (variableValorNumerico > 0 ) || (MiNumeroFav * TAU) 
+const valorSuma = MiNumeroFav + variableValorNumerico
 
+const valorResta = MiNumeroFav - variableValorNumerico
 
-// OPERADORES --------------------------------------------------------------------
+const valorMultiplicacion = MiNumeroFav * variableValorNumerico
 
-// 20.- variable 'valorSuma' cuyo valor sea la suma de MiNumeroFav y variableValorNumerico
-// const MiNumeroFav = 5
-// const variableValorNumerico = 1
-// const valorSuma = (MiNumeroFav + variableValorNumerico)
+const valorDivision = MiNumeroFav / 3
 
+// BUCLES 
 
-// 21.- Crear variable valorResta cuyo valor sea la resta de MiNumeroFav y variableValorNumerico
+let contarHasta10 = 0
+while ( contarHasta10 !== 10 ) {
+    ++contarHasta10
+ }
 
-// const MiNumeroFav = 5
-// const variableValorNumerico = 1
-// const valorResta = (MiNumeroFav - variableValorNumerico)
+let preI = 0
+let preJ = 0
+for (let i=0; i < 11; i++) {
+    preI = preI + ++preJ
+}
 
-
-// 22.- Crear variable valorMultiplicación cuyo valor sea la multiplicación de MiNumeroFav por variableValorNumerico
-// const MiNumeroFav = 5
-// const variableValorNumerico = 1
-// const valorMultiplicacion = (MiNumeroFav * variableValorNumerico)
-
-
-// 23.- Crear variable valorDivisión cuyo valor sea la división de MiNumeroFav entre 3
-// const MiNumeroFav = 5
-// const variableValorNumerico = 1
-// const valorDivision = (MiNumeroFav / 3)
-
-// BUCLES --------------------------------------------------------------------
-
-
-// 24.- Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle while hasta que se verifique que contarHasta10 === 10
-// let contarHasta10 = 0
-// while (contarHasta10 <= 10) {
-//     console.log(contarHasta10)
-//     contarHasta10++
-//  }
-
-
-// 25.- Crear las variables preI y preJ con valor 0 a continuación cree un bucle que itere 11 veces. En cada iteración se deberá sumar al valor de preI el valor de ++preJ
-// let preI = 0
-// let preJ = 0
-// for (i = 0; i < 11; i++) {
-//     preI = preI + ++preJ
-// }
-
-
-// 26.- Crear la variable sumaImpares con valor 0 a continuación crea un bucle que itere 10 veces (i < 10) si la iteración es impar se deberá sumar a sumaImpares el número de la iteración actual (i)
-// let sumaImpares = 0
-// for (let i = 0; i<=10; i++){
-//     if (( i % 2) != 0) {
-//         console.log(sumaImpares + i)
-//         }          
-//     }   
+let sumaImpares = 0
+for (let i=0; i < 10; i++) {
+    if ( i % 2 !== 0)
+    sumaImpares = sumaImpares + i
+}
